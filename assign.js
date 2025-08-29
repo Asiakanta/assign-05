88fc48dc4ce000d1dd34d213967d5bc2b6fb2987
Asiakanta <asiakanta1998@gmail.com>
2025-08-29 20:59:04 +0600

    added a new index file

--- assign.js
+++ assign.js
@@ -1,8 +1,4 @@
-// const amount = parseInt(document.getElementById("add-amount").value); or,
-
 let coins = 100;
-
-// const callHistoryData = [];
 
 const callButtons = document.querySelectorAll(".call-btn");
 const coinValue = document.getElementById("coin-value");
@@ -19,12 +15,6 @@ for (let Button of callButtons) {
       alert("NOT ENOUGH COINS!");
     }
   });
-
-  //   const data = {
-  //     name: "",
-  //     date: new Date().toLocaleTimeString(),
-  //   };
-  //   callHistoryData.push(data);
 }
 
 let heartValue = 0;
@@ -40,22 +30,21 @@ for (let heartIcon of heartIcons) {
     heartValue += 1;
     heartDisplay.innerText = heartValue;
   });
-
-  //   const data = {
-  //     name: "",
-  //     date: new Date().toLocaleTimeString(),
-  //   };
-  //   callHistoryData.push(data);
 }
+let copyValue = 0;
 
-// call history
-// const callHistoryData = [];
+const copyIcons = document.querySelectorAll(".copy-icon");
+const copyDisplay = document.getElementById("copyValue");
 
-// const data = {
-//   name: "",
-//   date: new Date().toLocaleTimeString(),
-// };
-// callHistoryData.push(data);
+// looping--------------------------->
+
+for (let copyIcon of copyIcons) {
+  copyIcon.addEventListener("click", function (e) {
+    e.preventDefault();
+    copyValue += 1;
+    copyDisplay.innerText = copyValue;
+  });
+}
 
 function makeCall(title, number) {
   let time = new Date().toLocaleTimeString();
